008dc1b6c5c4e2f1d2fc26f975fcba8f13c3ec1b
ali-heyhavand <alihwk213@gmail.com>
2025-11-01 20:48:10 +0330

    Fix BrowserRouter and prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./pages/home/Home"
-import Blog from "./pages/blog/Blog"
-import Course from "./pages/course/Course"
-import Panel from "./pages/panel/Panel"
-import Login from "./pages/login/Login"
-function App () {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/courses/:courseId" element={<Course/>}/>
-                <Route path="/blog" element={<Blog/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/panel" element={<Panel/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+import {Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import Course from "./pages/course/Course";
+import Panel from "./pages/panel/Panel";
+import Login from "./pages/login/Login";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/courses/:courseId" element={<Course />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/panel" element={<Panel />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"
+import App from "./App";
 import "bootstrap/dist/css//bootstrap.rtl.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import {ThemeProvider} from "react-bootstrap"
-import 'boxicons/css/boxicons.min.css';
-import "./index.css"
-
-
-
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { ThemeProvider } from "react-bootstrap";
+import "boxicons/css/boxicons.min.css";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThemeProvider dir="rtl">
-    <App/>
-  </ThemeProvider>
+  <BrowserRouter basename="/academic-project">
+    <ThemeProvider dir="rtl">
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
